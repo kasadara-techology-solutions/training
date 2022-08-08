62f03f8fb0025bf63cdb3e127f9673fa8edb9436
Ramya Nandhini <ramyanandhini.muralidharan@kasadaratech.com>
2022-08-08 11:30:36 +0530

    initial push

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Double dec = 9.2;
+            Console.WriteLine(Convert.ToDecimal(dec).GetType());
+        }
+    }
+}
